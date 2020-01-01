b36a66ae59fd79e7d0aef243f7c9a9fb676abfb5
suraj2502 <shuklasuraj620@gmail.com>
2020-01-02 00:39:45 +0530

    paypal button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,8 @@ export default class Cart extends Component {
                                     <Title name="your" title='cart'/>
                                 <CartColumns></CartColumns>  
                                 <CartList value={value}/>
-                           <CartTotals value={value} />     
+                           <CartTotals value={value} 
+                           history={this.props.history} />     
                                 </React.Fragment>
                                 
                             )
