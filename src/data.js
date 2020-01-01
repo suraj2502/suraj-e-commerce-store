f09113e1fd139a96d34f895d1d5f59f75dcdc290
suraj2502 <shuklasuraj620@gmail.com>
2020-01-02 01:36:05 +0530

    paypal button

--- src/data.js
+++ src/data.js
@@ -96,7 +96,19 @@ export const storeProducts = [
     inCart: false,
     count: 0,
     total: 0
-  }
+  },
+  {
+    id: 9,
+    title: "Iphone-11 pro",
+    img: "img/iphone11.png",
+    price: 90000,
+    company: "Apple",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
 ];
 
 export const detailProduct = {
